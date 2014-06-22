ae5f2276dbc60d794fd1ad221b44383988fbe1c0
chenwenpeng <chenwenpeng@meituan.com>
2014-06-22 15:43:42 +0800

    ［修改］修改提示信息

--- src/main/webapp/assets/js/js_common/FormPanel.js
+++ src/main/webapp/assets/js/js_common/FormPanel.js
@@ -28,6 +28,7 @@ CTA.common.SFormPanel = Ext.extend(Ext.FormPanel,{
 			        success : this._successCallback(settings),
 			        failure : function(action,response) {
 			        	response = Ext.util.JSON.decode(response.response.responseText);
+			        	
 			      	  	CTA.common.Mask.hideMask();
 				          var messages = response.messages;
 				          if(messages && messages.length>0){
@@ -38,8 +39,23 @@ CTA.common.SFormPanel = Ext.extend(Ext.FormPanel,{
 				            }
 				            Ext.Msg.alert('保存出错', msg);
 				            return;
+				          }else {
+				        	  //如果错误信息不存在，那么就认为是操作成功
+				        	  Ext.Msg.alert('成功提示', "操作成功，恭喜您！");
+				        	  if(Ext.getCmp("gridPanel")){
+					            	Ext.getCmp("gridPanel").getStore().reload();
+					            }
+					            if(Ext.getCmp("addWindow")){
+					            	Ext.getCmp("addWindow").close();
+					            }
+					            if(Ext.getCmp("updateWindow")){
+					            	Ext.getCmp("updateWindow").close();
+					            }
+					            if(Ext.getCmp("updateWindow_other")){
+					            	Ext.getCmp("updateWindow_other").close();
+					            }
 				          }
-				          Ext.Msg.alert('出错提示', "保存出错，如果重试后异常仍出现，请联系工程师解决!");
+				          //Ext.Msg.alert('出错提示', "保存出错，如果重试后异常仍出现，请联系工程师解决!");
 			      }
 			    },settings);
 		    	//提交，commit ---> submit 函数名不能一样，如果一样那么constructor.call 会覆盖掉 submit，导致提交方法失效
