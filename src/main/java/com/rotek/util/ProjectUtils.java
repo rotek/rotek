e0108a32d8b39687af495e77c2f6df02629573cd
springwjz <juzhu.w2013@gmail.com>
2014-07-10 11:09:14 +0800

    [修改]新增获取列名称的方法

--- src/main/java/com/rotek/util/ProjectUtils.java
+++ src/main/java/com/rotek/util/ProjectUtils.java
@@ -7,9 +7,14 @@ package com.rotek.util;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.cta.platform.config.SystemGlobals;
 
@@ -33,7 +38,7 @@ public class ProjectUtils {
 	* 
 	* 前提：表名格式  ：TB+四位工程编号+两位组编号
 	* 如：TB000101，表示0001号现场 ，01：表示泵组
-	* 
+	* 返回：现场编号集合：0001、0002。。。
 	* @return List<String>
 	* @throws Exception
 	* @author WangJuZhu
@@ -43,7 +48,7 @@ public class ProjectUtils {
 		List<String> pcodes = new ArrayList<String>();
 		//获取所有已TB开头的表的表名
 		String tabName = "TB%" ; 
-		List<String> tabNames = getTables(tabName);
+		List<String> tabNames = getTableNames(tabName);
 		if(tabNames.size() > 0){
 			for(String tn : tabNames){
 				pcodes.add(tn.substring(2, 6));
@@ -56,17 +61,17 @@ public class ProjectUtils {
 	/**
 	* @MethodName: getTables 
 	* @Description: 获取数据库中表的名称，表名称可以使用通配符,tabName为空则查全部表
+	* 如：tabName = "TB%",获取的是 TB开头的所有表的名称
+	* 通配符:单字符通配符("_"),多字符通配符("%")
 	* @return
 	* @author WangJuZhu
 	*/
-	public static List<String> getTables(String tabName){
+	public static List<String> getTableNames(String tabName){
 		List<String> dbNames = new ArrayList<String>() ;
-		Connection conn = null;
 		DatabaseMetaData md = null;
 		ResultSet rs = null;
 		try {
-			Class.forName(dbDriver);
-			conn = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+			Connection conn = getConnection();
 			md = conn.getMetaData();
 			rs = md.getTables(null, null, tabName, null);
 			while (rs.next()) {
@@ -80,6 +85,55 @@ public class ProjectUtils {
 		return dbNames ;
 	}
 	
+	/**
+	* @MethodName: getColumnNames 
+	* @Description: 根据表名获取其所有的字段名称
+	* @param tableName 表名称
+	* @return 字段名称List
+	* @author WangJuZhu
+	*/
+	public static List<String> getColumnNames(String tableName){
+		List<String> columnNames = new ArrayList<String>() ;
+		ResultSetMetaData data = null;
+		ResultSet rs = null;
+		if(StringUtils.isNotBlank(tableName)){
+			try {
+				Connection conn = getConnection();
+				String sql = "select * from " + tableName;
+				PreparedStatement stmt;
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery(sql);
+				data = rs.getMetaData();
+				for (int i = 1; i <= data.getColumnCount(); i++) {
+					columnNames.add(data.getColumnName(i));
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			return columnNames;
+		}else{
+			return null;
+		}
+	}
+	
+	/**
+	* @MethodName: getConnection 
+	* @Description: 获取数据库连接
+	* @return
+	* @author WangJuZhu
+	*/
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(dbDriver);
+			conn = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 	
 	/**
 	* @MethodName: removeRepetition 
@@ -107,15 +161,16 @@ public class ProjectUtils {
 	* @author WangJuZhu
 	*/
 	public static void main(String[] args) {
-		
-		String dbDrive = "com.mysql.jdbc.Driver" ; 
-		String url = "jdbc:mysql://58.221.61.123:3306/rotek?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8";  
-		String name = "root" ;  
-		String password = "threeNOODLES" ; 
+
+		String dbDrive = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://58.221.61.123:3306/rotek?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8";
+		String name = "root";
+		String password = "threeNOODLES";
 		Connection conn = null;
 		DatabaseMetaData md = null;
 		ResultSet rs = null;
-		int i = 1;
+		
+		int j = 1;
 		try {
 			Class.forName(dbDrive);
 			conn = DriverManager.getConnection(url, name, password);
@@ -123,12 +178,11 @@ public class ProjectUtils {
 			rs = md.getTables(null, null, "TB%", null);
 			System.out.println("+----------------+");
 			while (rs.next()) {
-				if (i == 1) {
+				if (j == 1) {
 					System.out.println("|库名：" + rs.getString(1));
 					System.out.println("+----------------+");
 				}
-				System.out.println("|表" + (i++) + ":"
-						+ rs.getString("TABLE_NAME"));
+				System.out.println("|表" + (j++) + ":" + rs.getString("TABLE_NAME"));
 			}
 			System.out.println("+----------------+");
 			rs.close();
@@ -136,7 +190,77 @@ public class ProjectUtils {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		 
+		try {
+			Class.forName(dbDrive); 
+			conn = DriverManager.getConnection(url, name, password);
+			String sql = "select * from r_button";
+			PreparedStatement stmt;
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+			ResultSetMetaData data = rs.getMetaData();
+			while (rs.next()) {
+				for (int i = 1; i <= data.getColumnCount(); i++) {
+					// 获得所有列的数目及实际列数
+					int columnCount = data.getColumnCount();
+					// 获得指定列的列名
+					String columnName = data.getColumnName(i);
+					// 获得指定列的列值
+					String columnValue = rs.getString(i);
+					// 获得指定列的数据类型
+					int columnType = data.getColumnType(i);
+					// 获得指定列的数据类型名
+					String columnTypeName = data.getColumnTypeName(i);
+					// 所在的Catalog名字
+					String catalogName = data.getCatalogName(i);
+					// 对应数据类型的类
+					String columnClassName = data.getColumnClassName(i);
+					// 在数据库中类型的最大字符个数
+					int columnDisplaySize = data.getColumnDisplaySize(i);
+					// 默认的列的标题
+					String columnLabel = data.getColumnLabel(i);
+					// 获得列的模式
+					String schemaName = data.getSchemaName(i);
+					// 某列类型的精确度(类型的长度)
+					int precision = data.getPrecision(i);
+					// 小数点后的位数
+					int scale = data.getScale(i);
+					// 获取某列对应的表名
+					String tableName = data.getTableName(i);
+					// 是否自动递增
+					boolean isAutoInctement = data.isAutoIncrement(i);
+					// 在数据库中是否为货币型
+					boolean isCurrency = data.isCurrency(i);
+					// 是否为空
+					int isNullable = data.isNullable(i);
+					// 是否为只读
+					boolean isReadOnly = data.isReadOnly(i);
+					// 能否出现在where中
+					boolean isSearchable = data.isSearchable(i);
+					System.out.println(columnCount);
+					System.out.println("获得列" + i + "的字段名称:" + columnName);
+					System.out.println("获得列" + i + "的字段值:" + columnValue);
+					System.out.println("获得列" + i + "的类型,返回SqlType中的编号:" + columnType);
+					System.out.println("获得列" + i + "的数据类型名:" + columnTypeName);
+					System.out.println("获得列" + i + "所在的Catalog名字:"+ catalogName);
+					System.out.println("获得列" + i + "对应数据类型的类:"+ columnClassName);
+					System.out.println("获得列" + i + "在数据库中类型的最大字符个数:"+ columnDisplaySize);
+					System.out.println("获得列" + i + "的默认的列的标题:" + columnLabel);
+					System.out.println("获得列" + i + "的模式:" + schemaName);
+					System.out.println("获得列" + i + "类型的精确度(类型的长度):" + precision);
+					System.out.println("获得列" + i + "小数点后的位数:" + scale);
+					System.out.println("获得列" + i + "对应的表名:" + tableName);
+					System.out.println("获得列" + i + "是否自动递增:" + isAutoInctement);
+					System.out.println("获得列" + i + "在数据库中是否为货币型:" + isCurrency);
+					System.out.println("获得列" + i + "是否为空:" + isNullable);
+					System.out.println("获得列" + i + "是否为只读:" + isReadOnly);
+					System.out.println("获得列" + i + "能否出现在where中:"+ isSearchable);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("数据库连接失败");
+		}
+
 	}
 	
 }
